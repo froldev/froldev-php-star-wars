a80845e40e638bb963de0307df3a6f823fa54c44
Fred OLIVE <frol.developpement@gmail.com>
2020-11-23 18:02:20 +0100

    section planet : add controller, manager and view

---
+++ src/Controller/PlanetController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\PlanetManager;
+
+/**
+ * Class PlanetController
+ * @package Controller
+ */
+class PlanetController extends AbstractController
+{
+
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function list() : string
+    {
+        $planetsManager = new PlanetManager();
+        $planets = $planetsManager->selectPlanet();
+
+        return $this->twig->render('Planet/list.html.twig', [
+          'planets' => $planets,
+          ]);
+    }
+
+     /**
+     * @param int $id
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function details(int $id)  : string
+    {
+      $planetManager = new PlanetManager();
+      $planet = $planetManager->selectOneById($id);
+
+      return $this->twig->render('Planet/details.html.twig', [
+        'planet' => $planet,
+      ]);
+    }
+
+
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function add() : string
+    {
+      $planetError = null;
+      if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $isValid = true;
+        if (empty($_POST['name']) || !isset($_POST['name'])) {
+          $planetError = "Merci de saisir un nom de planète";
+          $isValid = false;
+        }
+
+        if ($isValid) {
+          $planetManager = new PlanetManager();
+          if ($planetManager->insertPlanet($_POST)) {
+            header("Location:/planet/list");
+          }
+        }
+      }
+
+      return $this->twig->render('Planet/add.html.twig', [
+        'planetError' => $planetError,
+      ]);
+    }
+
+
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function edit(int $id) : string
+    {
+      if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $planetManager = new PlanetManager();
+        $planetManager->editPlanet($_POST, $id);
+        header('Location:/planet/list/');
+      }
+
+      $planetManager = new PlanetManager();
+      $planet = $planetManager->selectOneById($id);
+
+      return $this->twig->render('Planet/edit.html.twig', [
+        'planet' => $planet,
+      ]);
+    }
+
+
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function delete(int $id): void
+    {
+      $planetManager = new PlanetManager();
+      $planet = $planetManager->deletePlanet($id);
+      header('Location: /planet/list/'.$id);
+    }
+}
---
+++ src/Model/PlanetManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Model;
+
+class PlanetManager extends AbstractManager
+{
+
+    const TABLE = 'planet';
+
+
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+
+    public function selectPlanet(): array
+    {
+        $request = $this->pdo->query("SELECT * FROM " .self::TABLE. " ORDER BY name");
+        return $request->fetchAll();
+    }
+
+
+    public function insertPlanet(array $planet): bool
+    {
+        $request = $this->pdo->prepare("INSERT INTO " .self::TABLE. " (name, picture) VALUES (:name, :picture)");
+        $request->bindValue(':name', $planet['name'], \PDO::PARAM_STR);
+        $request->bindValue(':picture', $planet['picture'], \PDO::PARAM_STR);
+        return $request->execute();
+    }
+
+
+    public function editPlanet(array $planet, int $id): bool
+    {
+        $request = $this->pdo->prepare("UPDATE " . self::TABLE . " SET name=:name, picture=:picture WHERE " . self::TABLE . ".id=:id");
+        $request->bindValue(':id', $id, \PDO::PARAM_INT);
+        $request->bindValue(':name', $planet['name'], \PDO::PARAM_STR);
+        $request->bindValue(':picture', $planet['picture'], \PDO::PARAM_STR);
+        return $request->execute();
+    }
+
+
+    public function deletePlanet(int $id): void
+    {
+        $request = $this->pdo->prepare("DELETE FROM ".self::TABLE." WHERE id=:id");
+        $request->bindValue(":id", $id, \PDO::PARAM_INT);
+        $request->execute();
+    }
+
+}
