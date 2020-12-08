4a3faf94e60c5ee09e2b9450ab436a8ab46e8a3f
Fred OLIVE <frol.developpement@gmail.com>
2020-12-08 17:08:57 +0100

    section planet : modify form with management of controller and manager

--- src/Controller/PlanetController.php
+++ src/Controller/PlanetController.php
@@ -44,7 +44,6 @@ class PlanetController extends AbstractController
       ]);
     }
 
-
     /**
      * @return string
      * @throws \Twig\Error\LoaderError
@@ -53,9 +52,46 @@ class PlanetController extends AbstractController
      */
     public function add() : string
     {
-      $nameError = null;
+      $nameError = $pictureError = $file_destination = null;
       if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $isValid = true;
+
+        // download picture
+        if (!empty($_FILES['new-picture']['name']) && isset($_FILES['new-picture'])) {
+          $folder = 'planet';
+
+          $allowed = array('png', 'jpg', 'jpeg', 'gif');
+          $file_ext = explode('.', $_FILES['new-picture']['name']);
+          $file_ext = strtolower(end($file_ext));
+
+          $file_name_new = uniqid($folder.'-', false) . '.' . $file_ext;
+          $file_destination = 'assets/images/'.$folder.'/' . $file_name_new;
+
+          $filename = substr($_FILES['new-picture']['name'], 1);
+
+          if ($_FILES['new-picture']['size'] > 2097152) {
+            $pictureError = "La photo ne doit pas dépasser 2 Mo";
+            $isValid = false;
+          }
+          if(!in_array($file_ext, $allowed)) {
+            $pictureError = "La photo doit être au format jpg, jpeg, gif ou png";
+            $isValid = false;
+          }
+
+          if ($isValid) {
+            if (move_uploaded_file($_FILES['new-picture']['tmp_name'], $file_destination)) {
+              $_POST['picture'] = "/".$file_destination;
+              if (file_exists($filename)) {
+                unlink($filename);
+              }
+            } else {
+              $pictureError = "Erreur durant l'importation de la photo";
+              $isValid = false;
+            }
+          }
+        }
+        
+        // save data
         if (empty($_POST['name']) || !isset($_POST['name'])) {
           $nameError = "Merci de saisir un nom de planète";
           $isValid = false;
@@ -66,17 +102,16 @@ class PlanetController extends AbstractController
             $_POST["picture"] = self::EMPTY_PICTURE;
           }
           $planetManager = new PlanetManager();
-          if ($planetManager->insertPlanet($_POST)) {
-            header("Location:/planet/list");
-          }
+          $planetManager->insertPlanet($_POST);
+          header('Location:/planet/list/');
         }
       }
 
-      return $this->twig->render('Planet/add.html.twig', [
-        'nameError' => $nameError,
+      return $this->twig->render('Planet/edit.html.twig', [
+        'nameError'     => $nameError,
+        'pictureError'  => $pictureError,
       ]);
     }
-
 
     /**
      * @return string
@@ -86,59 +121,58 @@ class PlanetController extends AbstractController
      */
     public function edit(int $id) : string
     {
-      $nameError = $pictureError = null;
+      $nameError = $pictureError = $file_destination = null;
       if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $isValid = true;
-        
-        if (isset($_POST['modify-data'])) { // data
 
-          if (empty($_POST['name']) || !isset($_POST['name'])) {
-            $nameError = "Merci de saisir un nom de planète";
+        // download picture
+        if (!empty($_FILES['new-picture']['name']) && isset($_FILES['new-picture'])) {
+          $folder = 'planet';
+
+          $allowed = array('png', 'jpg', 'jpeg', 'gif');
+          $file_ext = explode('.', $_FILES['new-picture']['name']);
+          $file_ext = strtolower(end($file_ext));
+
+          $file_name_new = uniqid($folder.'-', false) . '.' . $file_ext;
+          $file_destination = 'assets/images/'.$folder.'/' . $file_name_new;
+
+          $filename = substr($_POST['picture'], 1);
+
+          if ($_FILES['new-picture']['size'] > 2097152) {
+            $pictureError = "La photo ne doit pas dépasser 2 Mo";
+            $isValid = false;
+          }
+          if(!in_array($file_ext, $allowed)) {
+            $pictureError = "La photo doit être au format jpg, jpeg, gif ou png";
             $isValid = false;
           }
 
           if ($isValid) {
-            $planetManager = new PlanetManager();
-            $planetManager->editDataPlanet($_POST, $id);
-            header('Location:/planet/list/');
-          }
-
-        } else { // picture
-
-          if (!empty($_FILES['new-picture']['name']) && isset($_FILES['new-picture'])) {
-            $folder = 'planet';
-
-            $allowed = array('png', 'jpg', 'jpeg', 'gif');
-            $file_ext = explode('.', $_FILES['new-picture']['name']);
-            $file_ext = strtolower(end($file_ext));
-
-            $file_name_new = uniqid($folder.'-', false) . '.' . $file_ext;
-            $file_destination = 'assets/images/'.$folder.'/' . $file_name_new;
-
-            $filename = substr($_POST['picture'], 1);
-
-            if ($_FILES['new-picture']['size'] > 2097152) {
-              $pictureError = "La photo ne doit pas dépasser 2 Mo";
-              $isValid = false;
-            }
-            if(!in_array($file_ext, $allowed)) {
-              $pictureError = "La photo doit être au format jpg, jpeg, gif ou png";
-              $isValid = false;
-            }
-
-            if ($isValid) {
-              if (move_uploaded_file($_FILES['new-picture']['tmp_name'], $file_destination)) {
-                $planetManager = new PlanetManager();
-                $planetManager->editPicturePlanet(['picture' => '/'.$file_destination], $id);
-                if (file_exists($filename)) {
-                  unlink($filename);
-                }
-                header('Location:/planet/edit/'.$id);
-              } else {
-                $pictureError = "Erreur durant l'importation de la photo";
+            if (move_uploaded_file($_FILES['new-picture']['tmp_name'], $file_destination)) {
+              $_POST['picture'] = "/".$file_destination;
+              if (file_exists($filename)) {
+                unlink($filename);
               }
+            } else {
+              $pictureError = "Erreur durant l'importation de la photo";
+              $isValid = false;
             }
           }
+        }
+        
+        // save data
+        if (empty($_POST['name']) || !isset($_POST['name'])) {
+          $nameError = "Merci de saisir un nom de planète";
+          $isValid = false;
+        }
+
+        if ($isValid) {
+          if (empty($_POST['picture']) || !isset($_POST['picture'])) {
+            $_POST["picture"] = self::EMPTY_PICTURE;
+          }
+          $planetManager = new PlanetManager();
+          $planetManager->editPlanet($_POST, $id);
+          header('Location:/planet/list/');
         }
       }
 
@@ -147,14 +181,13 @@ class PlanetController extends AbstractController
 
       return $this->twig->render('Planet/edit.html.twig', [
         'nameError'     => $nameError,
-        'pictureError'   => $pictureError,
+        'pictureError'  => $pictureError,
         'planet'        => $planet,
       ]);
     }
 
-
     /**
-     * @return string
+     * @return void
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
--- src/Model/PlanetManager.php
+++ src/Model/PlanetManager.php
@@ -7,19 +7,16 @@ class PlanetManager extends AbstractManager
 
     const TABLE = 'planet';
 
-
     public function __construct()
     {
         parent::__construct(self::TABLE);
     }
-
 
     public function selectPlanet(): array
     {
         $request = $this->pdo->query("SELECT * FROM " .self::TABLE. " ORDER BY name");
         return $request->fetchAll();
     }
-
 
     public function insertPlanet(array $planet): bool
     {
@@ -29,23 +26,14 @@ class PlanetManager extends AbstractManager
         return $request->execute();
     }
 
-
-    public function editPicturePlanet(array $planet, int $id): bool
+    public function editPlanet(array $planet, int $id): bool
     {
-        $request = $this->pdo->prepare("UPDATE " . self::TABLE . " SET picture=:picture WHERE " . self::TABLE . ".id=:id");
+        $request = $this->pdo->prepare("UPDATE " . self::TABLE . " SET name=:name, picture=:picture WHERE " . self::TABLE . ".id=:id");
         $request->bindValue(':id', $id, \PDO::PARAM_INT);
+        $request->bindValue(':name', $planet['name'], \PDO::PARAM_STR);
         $request->bindValue(':picture', $planet['picture'], \PDO::PARAM_STR);
         return $request->execute();
     }
-
-    public function editDataPlanet(array $planet, int $id): bool
-    {
-        $request = $this->pdo->prepare("UPDATE " . self::TABLE . " SET name=:name WHERE " . self::TABLE . ".id=:id");
-        $request->bindValue(':id', $id, \PDO::PARAM_INT);
-        $request->bindValue(':name', $planet['name'], \PDO::PARAM_STR);
-        return $request->execute();
-    }
-
 
     public function deletePlanet(int $id): void
     {
@@ -53,5 +41,4 @@ class PlanetManager extends AbstractManager
         $request->bindValue(":id", $id, \PDO::PARAM_INT);
         $request->execute();
     }
-
 }
